1ba2723dce0005b6a03b1feababe12c49e928aa7
hapi190601 <gc1206ro@gmail.com>
2021-02-13 09:56:50 +0000

    [0213-1900]

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -19,6 +19,7 @@ class BookersController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to show_path(@book.id)
     else
       @books = Book.all
@@ -31,16 +32,22 @@ class BookersController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to show_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to show_path
+      flash[:notice] = "Book was successfully updated"
+    else
+      render :edit
+    end
   end
 
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to index_path
+    flash[:notice] = "Book was successfully destroyed"
   end
 
 
